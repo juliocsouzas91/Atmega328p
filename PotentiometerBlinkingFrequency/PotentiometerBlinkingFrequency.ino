e64476ece11cc37df23fd5c1619b0c3055f1e7f0
Julio Souza <Juliocsouzas91@gmail.com>
2022-09-29 11:49:47 -0300

    Update PotentiometerBlinkingFrequency.ino

--- PotentiometerBlinkingFrequency/PotentiometerBlinkingFrequency.ino
+++ PotentiometerBlinkingFrequency/PotentiometerBlinkingFrequency.ino
@@ -6,24 +6,24 @@ Description - Changing the Blinking Frequency of the LED using a potentiometer*/
 #include <avr/interrupt.h>
 
 ISR(TIMER1_OVF_vect) {                             //interrupção do TIMER1 
-  TCNT1 = 0xC2F7;                                 // Renicia TIMER
-  PORTB ^= (1<<PORTB0);
+  TCNT1 = 0xACFF; // Load the timer (overflow in FFFF)
+  PORTB ^= (1<<PORTB0); //toogle the PortB0
 }
 
 int main(void){
-  DDRB |= 0b00000001; // Set output in port 5
-
+  DDRB |= 0b00000001; // Set output in port 1 in PB
+  /*Timer configuration TCCR1A and TCCR1B are two registers used to configure the timer 1 operation. It is set to overflow interruption*/
   TCCR1A &= 0b11111100; //WFM 11 and 10 set to zero
-  TCCR1B |= 0b00000101;
+  TCCR1B |= 0b00000101; //CS10 and CS12 set to 1 and CS11 to zero (clock frequency of the timer counter)
   TCCR1B &= 0b11100111; //WFM 13 and 12 set to zero
-  PORTB ^= (1<<PORTB0);
-  
-  TCCR1A = 0;
-  TCCR1B = 0;                        //limpa registrador
-  TCCR1B |= (1<<CS10)|(1 << CS12);   // configura prescaler para 1024: CS12 = 1 e CS10 = 1
-  TCNT1 = 0xC2F7;
-  TIMSK1 |= (1 << TOIE1);  
+
+  TCNT1 = 0xACFF; // Load the timer (overflow in FFFF)
+
+
+  TIMSK1 |= (1 << TOIE1);  //Enable interruptions
+  sei();
   SREG |= 0b10000000;
+  
   
   
   while(1){
